f028c3a4943a7f2845c61ce504492a19cc2783e3
Maxime BOSSARD <mxbossard@gmail.com>
2018-03-10 11:34:23 +0100

    Fix RGB colors, try to improve display performance.

--- povDisplay/test_improvment_rotDisplaySimul.py
+++ povDisplay/test_improvment_rotDisplaySimul.py
@@ -23,21 +23,37 @@ def makeCircle(numPoints, centerX, centerY, radius):
         for r in range(radius):
             x = (r + 1)*cos(angle) + centerX
             y = (r + 1)*sin(angle) + centerY
-            verts += [x,y]
+            verts += [int(round(x)),int(round(y))]
     #global circle
-    circle = pyglet.graphics.vertex_list(numPoints * radius + 1, ('v2f', verts))
+    #circle = pyglet.graphics.vertex_list(numPoints * radius + 1, ('v2f', verts))
+    circle = pyglet.graphics.vertex_list(floor(len(verts)/2), ('v2f', verts))
     return circle
+
+def makeCircle2(numPoints, centerX, centerY, radius):
+    verts = []
+    verts += [centerX, centerY]
+    for i in range(numPoints):
+        angle = radians(float(i)/numPoints * 360.0)
+        for r in range(radius):
+            x = (r + 1)*cos(angle) + centerX
+            y = (r + 1)*sin(angle) + centerY
+            verts += [int(round(x)),int(round(y))]
+    #global circle
+    return verts
 
 midWindowX = window.width / 2
 midWindowY = window.height / 2
 
-#imageMode = 'L'
-imageMode = 'RGB'
-ledScheme = [0 for i in range(10)] + [1 for i in range(16)] + [0] + [1 for i in range(16)]
+imageMode = 'L'
+#imageMode = 'RGB'
+#ledScheme = [0 for i in range(10)] + [1 for i in range(16)] + [0] + [1 for i in range(16)]
 #ledScheme = [0 for i in range(10)] + [1 for i in range(40)]
-ledDetails = 20 #20
+ledScheme = [1 for i in range(40)]
+ledDetails = 20
 ledRadius = 5 #2 #5
-angularPositionCount = 300 
+angularPositionCount = 64 
+#angularPositionCount = 300 
+#angularPositionCount = 600 
 ledCenterX = midWindowX
 ledCenterY = midWindowY
 
@@ -67,13 +83,14 @@ def get_circle_cartesian_coordinates(angle, radius):
     return (radius * cos(angle) + ledCenterX, radius * sin(angle) + ledCenterY)
 
 # Build all circles and sample image
-circlesMatrix=[[None for ledIndex in range(len(ledScheme) + 1)] for position in range(angularPositionCount)]
-imageSampleColor=[[None for ledIndex in range(len(ledScheme))] for position in range(angularPositionCount)]
+circlesMatrix = [[None for ledIndex in range(len(ledScheme) + 1)] for position in range(angularPositionCount)]
+imageSampleColor = [[None for ledIndex in range(len(ledScheme))] for position in range(angularPositionCount)]
 pixelCount = 0
 pixelSum = 0
 pixelMean = 128
 extremumColors = (255, 0)
 for position in range(len(circlesMatrix)):
+    barAllPixels = []
     for k, enable in enumerate(range(len(circlesMatrix[0]))):
         angle = pi * 2 / angularPositionCount * position
         radius = 2 * k * ledRadius
@@ -82,7 +99,7 @@ for position in range(len(circlesMatrix)):
         circle = makeCircle(ledDetails, coords[0], coords[1], ledRadius)
         circlesMatrix[position][k] = circle
         
-        if k >= len(ledScheme):
+        if k >= len(ledScheme) or not enable:
             continue
 
         imageX = round(k / len(ledScheme) * (imageSize/2) * cos(angle) + imageSize/2)
@@ -98,7 +115,20 @@ for position in range(len(circlesMatrix)):
         else:
             sampleColor = pixel #(pixel[0], pixel[1], pixel[2])
 
-        imageSampleColor[position][k] = sampleColor
+
+# Build led bars ready to display
+barsMap = {}
+for position in range(len(circlesMatrix)):
+    barAllPixels = []
+    for k, enable in enumerate(range(len(circlesMatrix[0]) - 1)):
+        if enable:
+            sampleColor = imageSampleColor[position][k]
+            if k <= pixelMean:
+                circle2 = makeCircle2(ledDetails, coords[0], coords[1], ledRadius)
+                barAllPixels += circle2
+
+    barsMap[position] = pyglet.graphics.vertex_list(floor(len(barAllPixels)/2), ('v2f', barAllPixels))
+
 
 @window.event
 def on_draw():
@@ -106,36 +136,49 @@ def on_draw():
 
     #if animPosition == 0:
     #    glClear(pyglet.gl.GL_COLOR_BUFFER_BIT)
+    #glClear(pyglet.gl.GL_COLOR_BUFFER_BIT)
 
     if imageMode == 'L':
-        rand = random.randint(ceil((extremumColors[0] + pixelMean)/2), floor((extremumColors[1] + pixelMean)/2)) 
+        rand = random.randint(ceil((extremumColors[0] + 4*pixelMean)/5), floor((extremumColors[1] + 4*pixelMean)/5)) 
 
-    for k, enable in enumerate(ledScheme):
-        circle = circlesMatrix[animPosition][k]
-        sampleColor = imageSampleColor[animPosition][k]
-        
-        if not sampleColor or not enable:
-            glColor3f(0, 0, 0)
+    #previousAnimPosition = (animPosition - 1) % angularPositionCount
+    #ledBar = barsMap[previousAnimPosition]
+    ledBar = barsMap[animPosition]
+    glColor3f(255, 0, 0)
+    ledBar.draw(GL_LINE_LOOP)
 
-        elif imageMode == 'L':
-            #if (sampleColor * (100 + rand) / 100) < pixelMean:
-            if sampleColor < rand:
-                glColor3f(255, 0, 0)
-            else:
-                glColor3f(0, 0, 0)
-        else:
-            glColor3f(sampleColor[0]/256, sampleColor[1]/256, sampleColor[2]/256)
+    #for k, enable in enumerate(ledScheme + [0]):
+    #    circle = circlesMatrix[previousAnimPosition][k]
+    #    glColor3f(0, 0, 0)
+    #    circle.draw(GL_LINE_LOOP)
 
-        circle.draw(GL_LINE_LOOP)
+    #for k, enable in enumerate(ledScheme):
+    #    circle = circlesMatrix[animPosition][k]
+    #    sampleColor = imageSampleColor[animPosition][k]
+    #    
+    #    if not sampleColor or not enable:
+    #        glColor3f(0, 0, 0)
+
+    #    elif imageMode == 'L':
+    #        #if (sampleColor * (100 + rand) / 100) < pixelMean:
+    #        if sampleColor < rand:
+    #            glColor3f(255, 0, 0)
+    #        else:
+    #            glColor3f(0, 0, 0)
+    #    else:
+    #        glColor3f(sampleColor[0]/256, sampleColor[1]/256, sampleColor[2]/256)
+
+    #    circle.draw(GL_LINE_LOOP)
 
     # Add white disks
-    circle = circlesMatrix[animPosition][k + 1]
+    #circle = circlesMatrix[animPosition][k + 1]
+    circle = circlesMatrix[animPosition][len(ledScheme)]
     glColor3f(255, 255, 255)
     circle.draw(GL_LINE_LOOP)
 
 glClear(pyglet.gl.GL_COLOR_BUFFER_BIT)
 pyglet.clock.set_fps_limit(None)
-pyglet.clock.schedule_interval(update_frames,1/10000.0)
+pyglet.clock.schedule_interval(update_frames,1/100.0)
 
 pyglet.app.run()
 
